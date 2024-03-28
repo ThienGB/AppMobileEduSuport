1da4ee91f78042c4f217d59f7f849f272036e1bf
PhunnMoc <phuongtrinhhoaian@gmail.com>
2024-03-29 02:10:07 +0700

    Them popup dialog trong file

--- EduSuport/app/src/main/java/com/example/edusuport/adapter/TaiLieuHocTapAdapter.java
+++ EduSuport/app/src/main/java/com/example/edusuport/adapter/TaiLieuHocTapAdapter.java
@@ -1,12 +1,16 @@
 package com.example.edusuport.adapter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -18,12 +22,14 @@ import java.util.ArrayList;
 
 public class TaiLieuHocTapAdapter  extends ArrayAdapter {
     Activity context;
+    private Context mContext;
     int resource;
     ArrayList<TaiLieuHocTap> List= new ArrayList<TaiLieuHocTap>();
 
     public TaiLieuHocTapAdapter(Context context, int resource, ArrayList<TaiLieuHocTap> list) {
         super(context, resource, list);
         List = list;
+        mContext = context;
     }
 
     @NonNull
@@ -37,6 +43,34 @@ public class TaiLieuHocTapAdapter  extends ArrayAdapter {
         TextView txtDate =(TextView) customView.findViewById(R.id.date_dangtailieu);
         txtTen.setText(List.get(position).getTenTaiLieu());
         txtDate.setText("Upload ngày"+List.get(position).getThoiGian());
+
+        ImageButton btn= customView.findViewById(R.id.btn_moreOptTaiLieuFile);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopUpDialog();
+            }
+        });
         return customView;
     }
+    private void showPopUpDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle("Chọn lựa");
+
+        // Thêm các lựa chọn vào danh sách
+        String[] options = {"Lựa chọn 1", "Lựa chọn 2", "Lựa chọn 3"};
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Xử lý khi một lựa chọn được chọn
+                String selectedOption = options[which];
+                Toast.makeText(mContext, "Bạn đã chọn: " + selectedOption, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Hiển thị AlertDialog
+        builder.create().show();
+    }
+
+
 }
