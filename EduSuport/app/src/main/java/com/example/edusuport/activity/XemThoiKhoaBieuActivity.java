5e0d9e0d8caf991d23868b2985c0849fc702a398
ThienGB <121418786+ThienGB@users.noreply.github.com>
2024-04-11 12:27:51 +0700

    Phuong temp (#5)
    * set các góc =10dp, chỉnh header p1
    
    * sửa 1/2 ( cứu t  :( )
    
    * suawr thoi khoa bieu xml
    
    * Thien sua loi manifest
    
    * Thien Sua loi xem TKB
    
    * Thien Sua loi xem TKB
    
    ---------
    
    Co-authored-by: PhunnMoc <phuongtrinhhoaian@gmail.com>

---
+++ EduSuport/app/src/main/java/com/example/edusuport/activity/XemThoiKhoaBieuActivity.java
@@ -0,0 +1,15 @@
+package com.example.edusuport.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.edusuport.R;
+
+public class XemThoiKhoaBieuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xem_thoi_khoa_bieu);
+    }
+}
