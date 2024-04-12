568b815ae78945e94371340517935f164563be8d
ThienGB <121418786+ThienGB@users.noreply.github.com>
2024-04-12 08:56:15 +0700

    Qanh (#8)
    * demo
    
    * Push nham code
    
    ---------
    
    Co-authored-by: qanh-ute <dinhquanganh03@gmail.com>

---
+++ EduSuport/app/src/main/java/com/example/edusuport/activity/NhapDiemChungActivity.java
@@ -0,0 +1,16 @@
+package com.example.edusuport.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.edusuport.R;
+
+public class NhapDiemChungActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nhap_diem_chung);
+    }
+}
